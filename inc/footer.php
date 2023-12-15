a1e133b957838217660bdbb4b70829aeb9ddb9bf
whitexiong <986247535@qq.com>
2023-12-15 18:30:12 +0800

    修改 footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,29 @@
-<br>
-<footer class="bg-dark p-4 mt-6 text-center text-white" >
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- 头部内容（例如标题，链接到CSS等） -->
+</head>
+<style>
+
+    html, body {
+        height: 100%;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .content {
+        flex: 1;
+    }
+
+</style>
+<body>
+<div class="content">
+    <!-- 页面的主要内容 -->
+</div>
+
+<footer class="bg-dark p-4 mt-6 text-center text-white">
     &copy; 2023 - 2024 - OLYMPICS
 </footer>
-
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -6,6 +6,8 @@
     <title>Olympics</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="../css/style.css" rel="stylesheet"> <!-- 引入自定义的CSS文件 -->
+    <script src="../js/common.js"></script> <!-- 引入自定义的JS文件 -->
   </body>
 </html>
 </head>
@@ -48,6 +50,8 @@
   </div>
 </nav>
 <div>
-<img src="img/olympics.png" class="img-fluid" style="max-width: 120%; height: auto;" alt="...">
+  <div class="d-flex justify-content-center">
+    <img src="img/olympics.png" class="img-fluid" alt="...">
+  </div>
 </div>
 </header>
