c65db6b2bc9a5d6fdf42decf9d36803d49dfadbc
whitexiong <986247535@qq.com>
2023-12-15 18:31:44 +0800

    修改比赛赛选

--- match.php
+++ match.php
@@ -3,54 +3,98 @@ include 'inc/fonction.php';
 
 
 
+// 准备并执行一个SQL查询，从'rencontre'表中选择所有记录。
 $requete = $pdo->prepare("SELECT * FROM rencontre");
 $requete->execute();
+// 从查询结果中获取所有行，并将它们存储在$matchs数组中。
 $matchs = $requete->fetchAll();
 
 
+// 获取当前时间
+$currentDateTime = new DateTime();
 
-// INSERTION
-// Determine if a variable is declared and is different than NULL isset( mixed $var [, mixed $... ]): bool
+// 根据筛选条件查询比赛
+$filterOption = $_GET['filter_option'] ?? '';
+
+if ($filterOption === 'upcoming') {
+    // 查询即将到来的比赛
+    $requete = $pdo->prepare("SELECT * FROM rencontre WHERE date_rencontre > ?");
+    $requete->execute([$currentDateTime->format('Y-m-d H:i:s')]);
+    $matchs = $requete->fetchAll();
+} elseif ($filterOption === 'no_upcoming') {
+    // 查询没有即将到来的比赛的球队
+    $requete = $pdo->prepare("
+        SELECT e.nom_equipe
+        FROM equipe e
+        WHERE NOT EXISTS (
+            SELECT 1 FROM rencontre r WHERE (r.id_equipe_a = e.id_equipe OR r.id_equipe_b = e.id_equipe) AND date_rencontre > ?
+        )
+    ");
+    $requete->execute([$currentDateTime->format('Y-m-d H:i:s')]);
+    $matchs = $requete->fetchAll();
+
+} else {
+    // 默认显示所有比赛
+    $requete = $pdo->prepare("SELECT * FROM rencontre");
+    $requete->execute();
+    $matchs = $requete->fetchAll();
+    // 在成功插入后重定向到'match.php'页面。
+
+}
+
+// 检查是否提交了表单。
 if (isset($_POST['submit'])){
+    // 准备一个SQL语句，用于向'rencontre'表中插入一条新记录。
+    $query = "INSERT INTO rencontre(lieu, type, id_equipe_a, id_equipe_b, date_rencontre) 
+              VALUES(:lieu, :type, :idEquipeA, :idEquipeB, :date)";
 
-    // SQL查询字符串，将数据插入到名为 "equipe" 的数据库表中
-    $query = "INSERT INTO rencontre(lieu, date_rencontre) VALUES(:lieu, :date)";
-    // 创建一个 PDO 预处理语句对象
+    // 创建一个预处理语句，以防止SQL注入。
     $stmt = $pdo->prepare($query);
-    // 这段代码是用于执行一个带有绑定参数的数据库查询，主要用于将从一个 HTML 表单通过 POST 方法提交的数据插入到数据库表中。
 
-    // 执行准备好的语句，将实际的数值绑定到占位符上，完成数据的插入
+    // 使用来自表单的值执行预处理语句。
+    // 确保为'type'、'idEquipeA'和'idEquipeB'提供的值在各自的表中有效且存在。
     $stmt->execute([
-    "lieu" => $_POST['lieu'], 
-    "date" => $_POST['dateRencontre'], 
+        "lieu" => $_POST['lieu'],
+        "type" => $_POST['type'], // 确保这个值在`type_discipline`表中存在
+        "idEquipeA" => $_POST['idEquipeA'], // 确保这个值有效且非空
+        "idEquipeB" => $_POST['idEquipeB'], // 确保这个值有效且非空
+        "date" => $_POST['dateRencontre'],
     ]);
-    // 在成功插入数据后，使用 header 函数进行页面重定向，将用户导向名为 "equipe.php" 的页面
-    header("location: match.php");
-    // 终止脚本的执行，确保在执行页面重定向后不再继续执行后续的 PHP 代码
-    exit;
 
-}else if(isset($_GET["action"])){
+    // 在成功插入后重定向到'match.php'页面。
+    header("location: match.php");
+    exit;
+} else if(isset($_GET["action"])){
+    // 从查询字符串中获取动作类型。
     $action = $_GET["action"];
 
+    // 根据动作类型处理不同的操作。
     switch($action){
         case "update":
+            // 准备并执行一个查询，从'rencontre'表中获取特定记录。
             $requete = $pdo->prepare("SELECT * FROM rencontre WHERE id_rencontre = ?");
             $requete->execute([$_GET['id_rencontre']]);
+            // 获取用于更新操作的记录。
             $matchUp = $requete->fetch();
 
             break;
-            case "delete":
-                $stmt = $pdo->prepare("DELETE FROM rencontre WHERE id_rencontre = ?");
-                $stmt->execute([$_GET['id_rencontre']]);
 
+        case "delete":
+            // 首先，删除'resultat_match'表中引用要删除的'id_rencontre'的任何记录。
+            $stmtDeleteResultatMatch = $pdo->prepare("DELETE FROM resultat_match WHERE id_rencontre = ?");
+            $stmtDeleteResultatMatch->execute([$_GET['id_rencontre']]);
+
+            // 然后，从'rencontre'表中删除该记录。
+            $stmtDeleteRencontre = $pdo->prepare("DELETE FROM rencontre WHERE id_rencontre = ?");
+            $stmtDeleteRencontre->execute([$_GET['id_rencontre']]);
+
+            // 在成功删除后重定向到'match.php'。
             header("location: match.php");
             exit;
     }
 }
+
+// 包含头部、视图和页脚的PHP文件。
 include 'inc/header.php';
 include 'view/vueMatch.phtml';
 include 'inc/footer.php';
-
-
-
-?>
